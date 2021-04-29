13c93491075d212faecdee3b4564cd6c55ef9bc4
Jade <jadeve.jimenez@yahoo.com>
2021-04-28 19:50:43 -0700

    python script filter, need to export as csv,not ready to merge

---
+++ static/filter.py
@@ -0,0 +1,17 @@
+import os
+import csv
+import pandas as pd
+
+data = pd.read_csv('data/Local_Area_Unemployment_Statistics__LAUS_.csv')
+
+data_yrs = data.loc[(data['Year'] >=2011)]
+
+#print(data_yrs.head())
+
+data_adjust = data_yrs.loc[(data_yrs['Seasonally Adjusted (Y/N)'] >= 'N')]
+
+#print(data_adjust)
+
+data_status = data_adjust.loc[(data_adjust['Status (Preliminary / Final)'] =="Final")]
+
+print(data_status)
